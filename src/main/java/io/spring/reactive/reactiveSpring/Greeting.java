68d93e48a1a2144873f691aad85b32becdf1e48c
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-19 16:44:30 -0400

    Updated application deployment and added test

--- src/main/java/io/spring/reactive/reactiveSpring/Greeting.java
+++ src/main/java/io/spring/reactive/reactiveSpring/Greeting.java
@@ -1,5 +1,9 @@
 package io.spring.reactive.reactiveSpring;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Objects;
+
 /**
  * Starting with Greeting POJO that will be serializes
  * as JSON by our RESTful service
@@ -7,6 +11,7 @@ package io.spring.reactive.reactiveSpring;
 public class Greeting {
     private String message;
 
+    @JsonCreator
     public Greeting(String message) {
         this.message = message;
     }
@@ -24,5 +29,18 @@ public class Greeting {
         return "Greeting{" +
                 "message='" + message + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Greeting greeting = (Greeting) o;
+        return Objects.equals(message, greeting.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
     }
 }
--- src/main/java/io/spring/reactive/reactiveSpring/ReactiveSpringApplication.java
+++ src/main/java/io/spring/reactive/reactiveSpring/ReactiveSpringApplication.java
@@ -1,13 +1,24 @@
 package io.spring.reactive.reactiveSpring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * Once the service has started, you can see the output at localhost:8080/hello
+ * The output comes from the reactive content being consumed by the
+ * WebClient.
+ */
 @SpringBootApplication
+@Slf4j
 public class ReactiveSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ReactiveSpringApplication.class, args);
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+        // We need to block for the content here or the JVM might exit before the message is logged
+        log.info(">> message = {} ", greetingClient.getMessage().block());
+    }
 
 }
