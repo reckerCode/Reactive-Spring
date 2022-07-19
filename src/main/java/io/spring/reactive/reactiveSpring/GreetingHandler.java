2ca1bfb6647f81bbb184e8ef6adf3c84e9d0d01a
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-19 15:57:08 -0400

    Using handler for request and response handling

--- src/main/java/io/spring/reactive/reactiveSpring/GreetingHandler.java
+++ src/main/java/io/spring/reactive/reactiveSpring/GreetingHandler.java
@@ -1,9 +1,22 @@
 package io.spring.reactive.reactiveSpring;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+/**
+ * In the Spring Reactive approach, we use a handler to handle
+ * request and create a response.
+ */
 @Component
 public class GreetingHandler {
-    public Mono
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters
+                        .fromValue(new Greeting("Hello, spring!")));
+    }
 }
